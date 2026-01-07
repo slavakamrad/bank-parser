f8788930bb6b031a4ed001fc6b123d87f22a543c
slavakamrad <slavakamrad@yandex.ru>
2026-01-07 23:28:34 +0300

    added bin and txt format

--- src/formats/bin_format.rs
+++ src/formats/bin_format.rs
@@ -1,1 +1,12 @@
+use crate::{ParserError, Result, Transaction};
+use std::io::{Read, Write};
 
+pub fn from_binary<R: Read>(_reader: R) -> Result<Vec<Transaction>> {
+    // TODO: Реализовать парсинг бинарного формата
+    Err(ParserError::BinaryFormat("Not implemented yet".to_string()))
+}
+
+pub fn to_binary<W: Write>(_transactions: &[Transaction], _writer: W) -> Result<()> {
+    // TODO: Реализовать запись в бинарный формат
+    Err(ParserError::BinaryFormat("Not implemented yet".to_string()))
+}
--- src/formats/txt_format.rs
+++ src/formats/txt_format.rs
@@ -1,1 +1,12 @@
+use crate::{ParserError, Result, Transaction};
+use std::io::{Read, Write};
 
+pub fn from_text<R: Read>(_reader: R) -> Result<Vec<Transaction>> {
+    // TODO: Реализовать парсинг текстового формата
+    Err(ParserError::TextFormat("Not implemented yet".to_string()))
+}
+
+pub fn to_text<W: Write>(_transactions: &[Transaction], _writer: W) -> Result<()> {
+    // TODO: Реализовать запись в текстовый формат
+    Err(ParserError::TextFormat("Not implemented yet".to_string()))
+}
